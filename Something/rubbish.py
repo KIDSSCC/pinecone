cd458fef44b79897ac434207110a7e81b641874f
KIDSSCC <18147667093@163.com>
2023-11-20 21:28:56 +0800

    somet rubbish

--- Something/rubbish.py
+++ Something/rubbish.py
@@ -1,34 +1,11 @@
-import numpy as np
+import subprocess as sp
 
-
-def gen_configs_recursively_fix(num_res:int, num_apps:int):
-    """
-    get a resource's allocation space.
-    input: resource id, num of groups/apps
-    return: a list contains all allocation plans of a resource
-    """
-    def gen_configs_recursively(u, num_res, a, num_apps):
-        if (a == num_apps - 1):
-            return None
-        else:
-            ret = []
-            for i in range(1, num_res - u + 1 - num_apps + a + 1):
-                confs = gen_configs_recursively(u + i, num_res, a + 1, num_apps)
-                if not confs:
-                    ret.append([i])
-                else:
-                    for c in confs:
-                        ret.append([i])
-                        for j in c:
-                            ret[-1].append(j)
-            return ret
-    res_config = gen_configs_recursively(0, num_res, 0, num_apps)
-    print(res_config)
-    for i in range(len(res_config)):
-        other_source = np.array(res_config[i]).sum()
-        res_config[i].append(num_res - other_source)
-    return res_config
-
-
-if __name__ =='__main__':
-    print(gen_configs_recursively_fix(5,3))
+if __name__ == '__main__':
+    print('prepare to test redis')
+    test_command = f"redis-benchmark -c 50 -n 200"
+    test_process = sp.Popen(test_command, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
+    test_process.wait()
+    print('test end')
+    stdout, stderr = test_process.communicate()
+    print("Standard Output:\n", stdout.decode())
+    print(type(stdout))
